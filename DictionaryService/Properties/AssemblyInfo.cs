3ff1aed987455e51b1029f3a987273cc9d410126
cyanray <a484599279@hotmail.com>
2021-10-21 22:20:03 +0800

    Update version properties.

--- DictionaryService/Properties/AssemblyInfo.cs
+++ DictionaryService/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("DictionaryService")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("LightDictionary - DictionaryService")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("cyanray")]
 [assembly: AssemblyProduct("DictionaryService")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
@@ -24,6 +24,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
 [assembly: ComVisible(false)]
