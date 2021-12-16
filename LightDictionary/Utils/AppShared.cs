a0a0b115cc7af485d8fbe925cbb157f8de163b8e
cyanray <a484599279@hotmail.com>
2021-12-16 14:20:39 +0800

    Rename: Constants --> AppShared.

--- LightDictionary/Utils/AppShared.cs
+++ LightDictionary/Utils/AppShared.cs
@@ -4,15 +4,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.ApplicationModel;
 
 namespace LightDictionary.Utils
 {
-    internal static class Constants
+    internal static class AppShared
     {
         public static BingLocalDictionaryService BingLocalDictionaryService = new BingLocalDictionaryService();
 
         public static BingOnlineDictionaryService BingOnlineDictionaryService = new BingOnlineDictionaryService();
 
         public static AppSettings AppSettings= new AppSettings();
+
+        public static string GetVersion()
+        {
+            PackageVersion version = Package.Current.Id.Version;
+            return $"{version.Major}.{version.Minor}.{version.Build}.{version.Revision}";
+        }
     }
 }
